800d6ca082b69b5d69dd9cf7db5288cf5ef0f583
called-d <called.0xd@gmail.com>
2018-03-08 18:18:02 +0900

    add: TestDictionaryTestCase

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,13 @@
 import unittest
 
+dictionary_file = "dict.json"
+
+class TestDictionaryTestCase(unittest.TestCase):
+    def test_dictionary_load(self):
+        import json
+        with open(dictionary_file) as f:
+            self.dic = json.load(f)
+        return self.dic
+
 if __name__ == "__main__":
     unittest.main()
